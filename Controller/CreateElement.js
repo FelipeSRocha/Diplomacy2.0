782d9af7eae4a8b579bbe3d4016c9cd1a9652c36
RochaFelpuda <felipe.21c@hotmail.com>
2022-04-30 14:53:00 -0300

    Diplomacy_V2

--- Controller/CreateElement.js
+++ Controller/CreateElement.js
@@ -1,15 +1,15 @@
 class AddElement{
 
-    AddElement(type, id_place, id, classname, text="",f, src){
+    AddElement(type, id_place="", id="", classname="", text="",f = ()=>{}, src=""){
         let place = document.getElementById(id_place)
         let el = document.createElement(type)
         el.innerHTML = text
         el.className = classname
         el.id = id
+        el.src = src
         el.addEventListener("click", () =>{
             f()
         })
-        el.src = src
         place.appendChild(el)
 
     }
--- View/view.js
+++ View/view.js
@@ -1,68 +1,82 @@
 class View{
     constructor(){
-        // this.Title = document.getElementById("Header")
-        // this.PlayersSection = document.getElementById("PlayersSection")
-        // this.ListofPlayers = Diplomacy.PlayerDB.Players
+        this.Title = document.getElementById("HeaderID")
+        this.Table = document.getElementById("ContainerID")
+        this.ListofPlayers = Diplomacy.PlayerDB.Players
     }
     RenderStartScreen(){
         this.RenderTitle()
-        // this.RenderPlayerSection()
-        // this.RenderTableSection()
+        this.RenderTable()
+        // this.RenderTableSection  ()
     }
 
     RenderTitle(){
         // Verifica se existe algo no titulo e exclui 
-        // if(document.querySelectorAll(".Header")){
-        //     console.log(document.querySelectorAll(".Header"))
-        //     this.Title.innerText = ''
-        // }
+        const DivExist = document.querySelectorAll("#HeaderID").length > 0
+        if(DivExist){
+            const element = document.getElementById('HeaderID');
+            element.remove()
+        }
         
         let header = new AddElement
-        header.AddElement("div","Game","Header","","", ()=>{})
+        header.AddElement("div","masterID","HeaderID","HeaderClass","", ()=>{})
 
-        let TitleSpace = new AddElement
-        TitleSpace.AddElement("div","Header","Title","TitleSpace","", ()=>{})
+        let Title = new AddElement
+        Title.AddElement("div","HeaderID","TitleID","TitleClass","", ()=>{})
+        
+        let Txt_Head = new AddElement
+        Txt_Head.AddElement("p","TitleID","Txt_HeadID","Txt_HeadClass","Diplomacy", ()=>{})
 
         let Btn_Head = new AddElement
-        Btn_Head.AddElement("div","Header","Btn_Header","Btn_Head","", ()=>{})
+        Btn_Head.AddElement("div","HeaderID","Btn_HeadID","Btn_HeadClass","", ()=>{})
 
         //Adiciona os botões Iniciais
-        // const btn_ResetGame = new AddElement
-        // btn_ResetGame.AddElement("button", `TitleSpace`, "btn_ResetGame", "Players_btn", "Reset Game",()=>{Diplomacy.ResetGame()})
+        const btn_ResetGame = new AddElement
+        btn_ResetGame.AddElement("button", `Btn_HeadID`, "btn_ResetGame", "Players_btn", "Reset Game",()=>{Diplomacy.ResetGame()}, "","flex:100%")
 
-        // const btn_RemovePlayer = new AddElement
-        // btn_RemovePlayer.AddElement("button", `TitleSpace`, "Btn_RemovePlayer", "Players_btn", 'Remove Player', () => {Diplomacy.PlayerDB.RemovePlayer()})
+        const btn_RemovePlayer = new AddElement
+        btn_RemovePlayer.AddElement("button", `Btn_HeadID`, "Btn_RemovePlayer", "Players_btn", 'Remove Player', () => {Diplomacy.PlayerDB.RemovePlayer()}, "", "flex:45%")
 
-        // const btn_AddPlayer = new AddElement
-        // btn_AddPlayer.AddElement("button",`TitleSpace`, "Btn_AddPlayer","Players_btn", 'Add Player',() => {Diplomacy.PlayerDB.AddPlayer()})
+        const btn_AddPlayer = new AddElement
+        btn_AddPlayer.AddElement("button",`Btn_HeadID`, "Btn_AddPlayer","Players_btn", 'Add Player',() => {Diplomacy.PlayerDB.AddPlayer()}, "", "flex:45%")
         
-        // const btn_NextRound = new AddElement
-        // btn_NextRound.AddElement("button", `TitleSpace`, "btn_NextRound", "Players_btn", "Next Round",()=>{Diplomacy.NextRound()})
+        const btn_NextRound = new AddElement
+        btn_NextRound.AddElement("button", `Btn_HeadID`, "btn_NextRound", "Players_btn", "Next Round",()=>{Diplomacy.NextRound()}, "", "flex:100%")
 
     }
 
-    RenderPlayerSection(){
+    RenderTable(){
+        this.RenderPlayers()
+        this.RenderMap()
+
+    }
+    RenderPlayers(){
         //Verifica se existe algo na parte dos jogadores e exclui 
-        if(document.querySelectorAll(".PlayerDiv")){
-            this.PlayersSection.innerText = ''
+        const DivExist = document.querySelectorAll("#ContainerID").length > 0
+        if(DivExist){
+            const element = document.getElementById('ContainerID');
+            element.remove()
         }
+
+        let container = new AddElement
+        container.AddElement("div","masterID","ContainerID","ContainerClass","",()=>{})
 
         //Itera pela lista de jogares para renderizar a interface de cada um
         this.ListofPlayers.forEach((el, i) =>{
-            let select = "Select Player"
 
             //Cria os elementos da interface do jogador
             let PlayerDiv = new AddElement
-            PlayerDiv.AddElement("div","PlayersSection", "PlayerDiv_" + (1+i),"PlayerDiv","",()=>{})
+            PlayerDiv.AddElement("div","ContainerID", `IDPlayerDiv_${(1+i)}`, `ClassPlayerDiv`)
 
-            let Playertitle = new AddElement
-            Playertitle.AddElement("h2", "PlayerDiv_" + (1+i), "PlayerTitle_" + (1+i),"PlayerTitle","Player " + (1+i),()=>{})
+            let PlayerStats = new AddElement
+            PlayerStats.AddElement("div",`IDPlayerDiv_${(1+i)}`, `IDPlayerStats_${(1+i)}`, `ClassPlayerStats`)
 
-            let PlayerProdDiv = new AddElement
-            PlayerProdDiv.AddElement("div","PlayerDiv_" + (1+i), "PlayerProd_" + (1+i),"PlayerProd","Produção",()=>{})
-
-            let PlayerProdBank = new AddElement
-            PlayerProdBank.AddElement("div","PlayerDiv_" + (1+i),"PlayerBank_" + (1+i),"PlayerBank","Banco",()=>{})
+            let PlayerTitle = new AddElement
+            PlayerTitle.AddElement("h1",`IDPlayerStats_${(1+i)}`, `IDPlayerTitle_${(1+i)}`, `ClassPlayerTitle`, `Player ${i+1}`)
+            let PlayerProd = new AddElement
+            PlayerProd.AddElement("div",`IDPlayerStats_${(1+i)}`, `IDPlayerProd_${(1+i)}`, `ClassPlayerProd`)
+            let PlayerBank = new AddElement
+            PlayerBank.AddElement("div",`IDPlayerStats_${(1+i)}`, `IDPlayerBank_${(1+i)}`, `ClassPlayerBank`)
 
             //Itera pelos stats dos jogadores
             Object.keys(el).forEach(key =>{
@@ -74,26 +88,25 @@ class View{
                     case "ProdComida":
                     case "ProdExercito":
                     case "ProdTecnologia":
-                        let ProdDiv = new AddElement
-                        ProdDiv.AddElement("div","PlayerProd_" + (1+i), `PlayerProd_${key}_${(1+i)}`, "PlayerProd_each")
+                        let ProdDivEach = new AddElement
+                        ProdDivEach.AddElement("div",`IDPlayerProd_${(1+i)}`, `IDPlayerProd_${key}_${(1+i)}`, "ClassPlayerProd_each", )
 
                         let img_Prod = new AddElement
                         let src_prod = `/Img/${key}.png`
-                        img_Prod.AddElement("img",`PlayerProd_${key}_${(1+i)}`, `img_${key}_${(1+i)}`, "img_resources","",() =>{}, src_prod)
-
-                        let RightDivProd = new AddElement
-                        RightDivProd.AddElement("div",`PlayerProd_${key}_${(1+i)}`, `RightDivProd_${key}_${(1+i)}`, "RightDivProd", text,()=>{})
+                        img_Prod.AddElement("img",`IDPlayerProd_${key}_${(1+i)}`, "", "img_resources","",() =>{}, src_prod)
 
                         let PlayerProd = new AddElement
                         text = `+ ${el[key]}`
-                        PlayerProd.AddElement("p",`RightDivProd_${key}_${(1+i)}`, "", "ResourcesText", text,()=>{})
+                        PlayerProd.AddElement("p",`IDPlayerProd_${key}_${(1+i)}`, "", "ResourcesText", text,()=>{})
 
-                        let btn_decrease_prod = new AddElement
-                        btn_decrease_prod.AddElement("button", `RightDivProd_${key}_${(1+i)}`, "", "Players_btn_resources_Dec", "-1",()=>{Diplomacy.PlayerDB.Players[i].ModifyValue(key,-1)})
-                        let btn_increase_prod = new AddElement
-                        btn_increase_prod.AddElement("button", `RightDivProd_${key}_${(1+i)}`, "", "Players_btn_resources_Inc", "+1",()=>{Diplomacy.PlayerDB.Players[i].ModifyValue(key,1)})
-
-
+                        let btnProd_decrease_1 = new AddElement
+                        btnProd_decrease_1.AddElement("button", `IDPlayerProd_${key}_${(1+i)}`, "", "Dec1", "-1",()=>{Diplomacy.PlayerDB.Players[i].ModifyValue(key,-1)})
+                        let btnProd_increase_1 = new AddElement
+                        btnProd_increase_1.AddElement("button", `IDPlayerProd_${key}_${(1+i)}`, "", "Inc1", "+1",()=>{Diplomacy.PlayerDB.Players[i].ModifyValue(key,1)})
+                        let btnProd_decrease_5 = new AddElement
+                        btnProd_decrease_5.AddElement("button", `IDPlayerProd_${key}_${(1+i)}`, "", "Dec5", "-5",()=>{Diplomacy.PlayerDB.Players[i].ModifyValue(key,-5)})
+                        let btnProd_increase_5 = new AddElement
+                        btnProd_increase_5.AddElement("button", `IDPlayerProd_${key}_${(1+i)}`, "", "Inc5", "+5",()=>{Diplomacy.PlayerDB.Players[i].ModifyValue(key,5)})
                         break
 
                     //Renderiza a parte do banco do jogador
@@ -101,51 +114,37 @@ class View{
                     case "Comida":
                     case "Exercito":
                     case "Tecnologia":
-                        let BankDiv = new AddElement
-                        BankDiv.AddElement("div","PlayerBank_" + (1+i), `PlayerBank_${key}_${(1+i)}`, "PlayerBank_each")
+                        let BankDivEach = new AddElement
+                        BankDivEach.AddElement("div",`IDPlayerBank_${(1+i)}`, `IDPlayerBank_${key}_${(1+i)}`, "ClassPlayerBank_each")
 
                         let img_Bank = new AddElement
                         let src_Bank = `/Img/${key}.png`
-                        img_Bank.AddElement("img",`PlayerBank_${key}_${(1+i)}`, `img_${key}_${(1+i)}`, "img_resources","",() =>{}, src_Bank)
-
-                        let RightDivBank = new AddElement
-                        RightDivBank.AddElement("div",`PlayerBank_${key}_${(1+i)}`, `RightDivBank_${key}_${(1+i)}`, "RightDivBank", text,()=>{})
+                        img_Bank.AddElement("img",`IDPlayerBank_${key}_${(1+i)}`, "", "img_resources","",() =>{}, src_Bank)
 
                         let PlayerBank = new AddElement
                         text = `+ ${el[key]}`
-                        PlayerBank.AddElement("p",`RightDivBank_${key}_${(1+i)}`, "", "ResourcesText", text,()=>{})
+                        PlayerBank.AddElement("p",`IDPlayerBank_${key}_${(1+i)}`, "", "ResourcesText", text,()=>{})
 
-                        let btn_decrease5_bank = new AddElement
-                        btn_decrease5_bank.AddElement("button", `RightDivBank_${key}_${(1+i)}`, "menos5", "Players_btn_resourcesBank_Dec", "-5",()=>{Diplomacy.PlayerDB.Players[i].ModifyValue(key,-5)})
-                        let btn_decrease_bank = new AddElement
-                        btn_decrease_bank.AddElement("button", `RightDivBank_${key}_${(1+i)}`, "menos1", "Players_btn_resourcesBank_Dec", "-1",()=>{Diplomacy.PlayerDB.Players[i].ModifyValue(key,-1)})
-                        let btn_increase_bank = new AddElement
-                        btn_increase_bank.AddElement("button", `RightDivBank_${key}_${(1+i)}`, "mais1", "Players_btn_resourcesBank_Inc", "+1",()=>{Diplomacy.PlayerDB.Players[i].ModifyValue(key,1)})
-                        let btn_increase5_bank = new AddElement
-                        btn_increase5_bank.AddElement("button", `RightDivBank_${key}_${(1+i)}`, "mais5", "Players_btn_resourcesBank_Inc", "+5",()=>{Diplomacy.PlayerDB.Players[i].ModifyValue(key,5)})
-
+                        let btnBank_decrease_1 = new AddElement
+                        btnBank_decrease_1.AddElement("button", `IDPlayerBank_${key}_${(1+i)}`, "", "Dec1", "-1",()=>{Diplomacy.PlayerDB.Players[i].ModifyValue(key,-1)})
+                        let btnBank_increase_1 = new AddElement
+                        btnBank_increase_1.AddElement("button", `IDPlayerBank_${key}_${(1+i)}`, "", "Inc1", "+1",()=>{Diplomacy.PlayerDB.Players[i].ModifyValue(key,1)})
+                        let btnBank_decrease_5 = new AddElement
+                        btnBank_decrease_5.AddElement("button", `IDPlayerBank_${key}_${(1+i)}`, "", "Dec5", "-5",()=>{Diplomacy.PlayerDB.Players[i].ModifyValue(key,-5)})
+                        let btnBank_increase_5 = new AddElement
+                        btnBank_increase_5.AddElement("button", `IDPlayerBank_${key}_${(1+i)}`, "", "Inc5", "+5",()=>{Diplomacy.PlayerDB.Players[i].ModifyValue(key,5)})
                         break
                     
-                    //Renderiza o jogador selecionado
-                    // case "selected":
-                    //     if (el[key]){
-                    //         let selected = document.getElementById("PlayerDiv_" + (1+i))
-                    //         selected.style.backgroundColor = "green"
-                    //         select = 'Diselect Player'
-                    //     }
-                    //     break
                 }
 
             })
             
-            // let btn_SelectPlayer = new AddElement
-            // btn_SelectPlayer.AddElement("button", `PlayerTitle_${i+1}`, `Btn_SelectPlayer_${i+1}`, "Players_btn", select, () => {Diplomacy.PlayerDB.VerifySelectedPlayers(i)})
         })
-
     }
-
-    RenderTableSection(){
+    RenderMap(){
         const Table = new AddElement
-        Table.AddElement("div", `PlayersSection`, "Game1", "GameDiv", "O Jogo",()=>{})
+        Table.AddElement("div", `masterID`, "IDTable", "ClassTable", "",()=>{},)
+        const ImgTable = new AddElement 
+        ImgTable.AddElement("img", `IDTable`, "IDImgTable", "ClassImgTable", "O Jogo",()=>{},"Img/O jogo2.png")
     }
 }
