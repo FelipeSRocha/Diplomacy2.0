2e507a1c0705512f4db47f2bf54248cd6b6a36b3
RochaFelpuda <felipe.21c@hotmail.com>
2022-05-12 12:54:46 -0300

    npm, express e nodemon

--- app.js
+++ app.js
@@ -1,35 +1,9 @@
-// const http = require("http")
-// const fs = require('fs')
-// const port = 3000
-
-// const server = http.createServer(function(req, res) {
-//     res.writeHead(200, {'Content-Type': 'text/html'})
-//     fs.readFile('index.html', function(error, data){
-//         if (error){
-//             res.writeHead(404)
-//             res.write("Error: File Not Found")
-//         }else{
-//             res.write(data)
-//         }
-//         res.end
-//     }) 
-// })
-
-// server.listen(port, function(error){
-//     if (error){
-//         console.log("Erro")
-//     } else{
-//         console.log(`Server ouvindo na porta ${port}`)
-//     }
-// })
-
 const express = require('express')
+let routesIndex = require('./routes/index')
+
 const app = express()
 
-app.get('/', function(req, res){
-    res.send("Seja bem Vindo")
-    // res.sendFile('E:/Felipe/JS/Projeto Diplomacy/index.html')
-})
+app.use(routesIndex)
 
 app.listen(3000, function(){
     console.log("Servidor rodando na url http://localhost:3000")
